0b1ff9e4e1b8a8391963ad459653185d57b5498d
Emre Kılınç <emre@nitro.io>
2012-11-27 22:12:59 +0200

    Basic authentication for API

--- app.rb
+++ app.rb
@@ -4,7 +4,15 @@ require 'json'
 class ExceptionResource < Sinatra::Base
   set :methodoverride, true
 
+  helpers do
+    def valid_key? key
+      key == "123"
+    end
+  end
+
   get '/exceptions' do
+    p params[:key]
+    error 401 unless valid_key?(params[:key])
     content_type :json
 
     test_data = [
