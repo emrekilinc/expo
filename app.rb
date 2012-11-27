ee3fc4b6ce7f29a057c023467789b9587acaa00f
Emre Kılınç <emre@nitro.io>
2012-11-27 22:19:24 +0200

    Implement API structure

--- app.rb
+++ app.rb
@@ -1,26 +1,36 @@
 require 'sinatra'
 require 'json'
 
+VALID_KEY = "123"
+
 class ExceptionResource < Sinatra::Base
   set :methodoverride, true
 
   helpers do
+    # Checks the query string or
+    # post params for valid key
     def valid_key? key
-      key == "123"
+      key == VALID_KEY
+    end
+
+    # Returns a JSON object with status
+    # code and explanation in it
+    def json_status code, reason
+      {
+        :status => code,
+        :reason => reason
+      }.to_json
     end
   end
 
-  get '/exceptions' do
-    p params[:key]
+  # Check the key before every route
+  before do
     error 401 unless valid_key?(params[:key])
-    content_type :json
+  end
 
-    test_data = [
-      { "key" => "value" },
-      { "key2" => "value2" }
-    ]
-
-    test_data.to_json
+  ## GET : '/' 
+  get '/' do
+    json_status 404, "No such route has been found"
   end
 
 end
